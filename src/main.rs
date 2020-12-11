fa11d25fb70356a1ef86345afce6875662317ec1
Nathan <terakilobyte@gmail.com>
2020-12-11 09:33:27 -0800

    more setup

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
 fn main() {
     println!("Hello, world!");
+    println!("Hell yeah it works!");
+    println!("this should cause the previous line to error")
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert_eq!(2, 2);
+        println!("foo!@")
+    }
 }
